8ae012a5efa70cffdace05ddba8a05d0add37dd9
Ali Bayati <bayati.ali@icloud.com>
2018-05-24 08:41:35 -0500

    making another branch

--- apps/atApt/urls.py
+++ apps/atApt/urls.py
@@ -1,4 +1,19 @@
 from django.conf.urls import url,include
+from . import views
+
+
 urlpatterns = [
-    
+    url(r'^$',views.index),
+    url(r'^main$',views.main),
+    url(r'^customer_register$',views.customer_register),
+    url(r'^staff_register$',views.staff_register),
+    url(r'^login$',views.login),
+    url(r'^logout$',views.logout),
+    url(r'^add_appointment$',views.add_appointment),
+    url(r'^user_page&',views.user_page),
 ]
+
+    # url(r'^travel$',views.dashboard),
+    # url(r'^create_plan$',views.create_plan),
+    # url(r'^destination/(?P<trip_id>\d+)/$',views.trip_view),
+    # url(r'^join_trip/(?P<trip_id>\d+)/$',views.join_trip),
--- apps/atApt/views.py
+++ apps/atApt/views.py
@@ -1,4 +1,67 @@
 from django.shortcuts import render,redirect
+from .models import Users,Appointments
+from django.contrib import messages
+
+
 
 def index(request):
-    return redirect('/')
+
+    return redirect('/main')
+
+
+
+def main(request):
+    print("home page")
+    
+    return False
+    # return render(request,'{{main-view}}/index.html')
+
+def user_page(request):
+    id=request.session['logged']
+    user=Users.objects.get(id=id)
+    # return render(request,'{{user-page-view}}/index.html')
+
+    return False
+
+
+def staff_register(request):
+    print ("registering staff")
+    if  Users.objects.staff_register(request):
+        print ('staff created')
+        messages.add_message(request,messages.INFO,"Staff Added")
+        return redirect('/main')
+    else:
+        return redirect('/main')
+
+
+
+def customer_register(request):
+    print ("registering customer")
+    if  Users.objects.customer_register(request):
+        print ('customer created')
+        
+        messages.add_message(request,messages.INFO,"Customer Added")
+        return redirect('/main')
+    else:
+        return redirect('/main')
+
+
+    
+def login(request):
+    
+    if(Users.objects.login(request)):
+        print(request.session['logged'])
+        # return redirect('/travel')
+        return False
+    else:
+        return redirect('/main')
+
+def logout(request):
+    request.session['logged']=""
+    return redirect('/main')
+
+
+def add_appointment(request):
+    print("adding an appointment")
+    Users.objects.add_appointment(request)
+    return False
