c90cee55bf55d098a964fcd1b4f63b09d1daad91
Ali Bayati <bayati.ali@icloud.com>
2018-05-30 15:19:54 -0500

    last commit befor deploy

--- apps/atApt/models.py
+++ apps/atApt/models.py
@@ -163,3 +163,4 @@ class Appointments(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     objects=AppointmentsManager()
+    
